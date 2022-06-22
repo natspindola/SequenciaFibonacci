4b4fdbdc261e922657c71a05c7653f3efd4f7c61
Perk <natspindola@hotmail.com>
2022-06-22 20:36:47 -0300

    Add project files.

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SequenciaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indiceUltimoElemento = 1;
+            int indicePenultimoElemento = 0;
+
+            var sequenciaFibonacci = new List<int> { 0, 1 };
+
+            Console.WriteLine("Digite um numero: ");
+
+            var numeroInformado = int.Parse(Console.ReadLine());
+
+            var pararWhile = true;
+
+            while (pararWhile)
+            {
+                var proximoElemento = sequenciaFibonacci[indicePenultimoElemento] + sequenciaFibonacci[indiceUltimoElemento];
+
+                sequenciaFibonacci.Add(proximoElemento);
+
+                indiceUltimoElemento++;
+                indicePenultimoElemento++;
+
+                if (numeroInformado <= proximoElemento)
+                    pararWhile = false;
+            }
+
+            if(sequenciaFibonacci.Any(a=> a == numeroInformado))
+                Console.WriteLine("É um número da Sequência Fibonacci.");
+            else
+                Console.WriteLine("Não um número da Sequência Fibonacci.");
+        }
+    }
+}
